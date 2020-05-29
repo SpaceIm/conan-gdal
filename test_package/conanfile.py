3afbe65ec6361984a8daa725f9ee6eb1345ed35d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-29 21:19:44 +0200

    run gdal_translate in test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,6 +13,7 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
+            self.run("gdal_translate --formats", run_environment=True)
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
             bin_path_c = os.path.join("bin", "test_package_c")
